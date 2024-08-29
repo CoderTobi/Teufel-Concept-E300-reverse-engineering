3120de49f30fcc2e4074b0d8f60bda83cc38124e
Tobias Graf <tobiasgraf03@gmail.com>
2024-08-29 02:14:20 +0200

    I2C Vorbereitung - PinMode Setup vor WLAN - I2C Setup hinzugefügt - I2C Methoden angelegt - I2C noch nicht getestet - Unsicher ob ESP32 64kHz kann

--- Arduino Code/Subwoofer/Subwoofer.ino
+++ Arduino Code/Subwoofer/Subwoofer.ino
@@ -1,8 +1,9 @@
 #include <WiFi.h>
 #include <ArduinoHA.h>
+#include <Wire.h>
 #include "secrets.hpp"
 
-// Config
+// Pin Config
 const uint8_t CD_PIN = 23; // Zweck unbekannt - aktuell unused
 const uint8_t SDA_PIN = 15;
 const uint8_t SCL_PIN = 2;
@@ -19,12 +20,16 @@ HANumber numberAll("VolAll");
 HANumber numberBass("VolBass");
 HASwitch switchPower("Power");
 
+// ===================== Call Backs ========================
+
 void onSwitchCommand(bool state, HASwitch* sender)
 {
   Serial.print("Standby is now: ");
   Serial.println(state);
 
-  
+  i2c_write_sep();
+  i2c_write_power(state);
+
   sender->setState(state); // report state back to the Home Assistant
 }
 
@@ -58,6 +63,41 @@ void onVolBassChange(HANumeric number, HANumber* sender)
   sender->setState(number); // report the selected option back to the HA panel
 }
 
+// ===================================== I2C ===================================
+
+void i2c_write_sep()
+{
+  Wire.beginTransmission(0x46);
+  Wire.write(0xC0);
+  Wire.endTransmission();
+}
+
+void i2c_write_power(bool on)
+{
+  Wire.beginTransmission(0x46);
+  if(on)
+  {
+    Wire.write(0xF8);
+  }
+  else
+  {
+    Wire.write(0xF9);
+  }
+  Wire.endTransmission();
+}
+
+void i2c_write_all(uint8_t value)
+{
+
+}
+
+void i2c_write_ch(uint8_t value, uint8_t reg)
+{
+
+}
+
+// ===================================== Setup ===================================
+
 void setup() 
 {
   // Device Setup
@@ -70,6 +110,17 @@ void setup()
   Serial.begin(9600);
   Serial.print("Wifi"); 
 
+  // PIN Mode Setup
+  pinMode(ST_PIN, OUTPUT);
+  pinMode(MUTE_PIN, OUTPUT);
+  //pinMode(CD_PIN, OUTPUT);
+  digitalWrite(ST_PIN, LOW);
+  digitalWrite(MUTE_PIN, LOW);
+
+  // I2C Setup
+  Wire.begin(SDA_PIN, SCL_PIN);
+  Wire.setClock(64000); // Original arbeitet bei 64kHz - unsicher ob ESP kann langsamer als 100kHz
+
   // Wifi verbinden
   WiFi.begin(WIFI_SSID, WIFI_PW);
   while (WiFi.status() != WL_CONNECTED) {
@@ -77,13 +128,6 @@ void setup()
     delay(500); // waiting for the connection
   }
   Serial.println("connected"); 
-
-  // PIN Mode Setup
-  pinMode(ST_PIN, OUTPUT);
-  pinMode(MUTE_PIN, OUTPUT);
-  //pinMode(CD_PIN, OUTPUT);
-  digitalWrite(ST_PIN, LOW);
-  digitalWrite(MUTE_PIN, LOW);
 
   // Device Types Setup
   switchPower.onCommand(onSwitchCommand);
@@ -99,6 +143,8 @@ void setup()
   // MQTT broker connection (muss ans Ende von Setup)
   mqtt.begin(MQTT_HOST, MQTT_USER, MQTT_PW);
 }
+
+// ===================================== Loop ===================================
 
 void loop() {
   mqtt.loop();
