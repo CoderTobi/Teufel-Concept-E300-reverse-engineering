bb22c199bcb6244a029fc02a1fd9e81368fde319
Tobias Graf <tobiasgraf03@gmail.com>
2024-09-13 01:39:21 +0200

    Ursache gefunden - 3,3V nicht genug für MUTE - 5V mit 10k Vorwiderstand auch nicht genug - 5V mit 1k Vorwiderstand funktioniert - Perfboard ändern und weiteren NPN einfügen

--- Arduino Code/Subwoofer_Emulator/Subwoofer_Emulator.ino
+++ Arduino Code/Subwoofer_Emulator/Subwoofer_Emulator.ino
@@ -1,10 +1,11 @@
 #include <Wire.h>
 
 void setup() {
-  Wire.begin(0x46); // Start I2C Bus als Slave mit der Adresse 0x46
-  Wire.setClock(64000);
+  //Wire.begin(0x46); // Start I2C Bus als Slave mit der Adresse 0x46
+  Wire.begin(0x44); // Start I2C Bus als Slave mit der Adresse 0x44
+  Wire.setClock(50000); // Original arbeitet bei 64kHz
   Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600); // Starten der seriellen Kommunikation mit einer Baudrate von 9600
+  Serial.begin(115200); // Starten der seriellen Kommunikation
 }
 
 void loop() {
@@ -17,7 +18,7 @@ void receiveEvent(int bytes) {
   while(Wire.available()) {
     uint8_t data = Wire.read(); // Empfangen von einem Byte als Zeichen
     //Serial.print("HEX: ");
-    //Serial.println(data, HEX); // Ausgabe des Wertes in HEX
+    Serial.println(data, HEX); // Ausgabe des Wertes in HEX
     //Serial.print("BIN: ");
     //Serial.println(data, BIN); // Ausgabe des Wertes in BIN
     //Serial.print("DEC: ");
